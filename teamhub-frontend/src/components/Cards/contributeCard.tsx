bdfbeb9eda587b5a4a99418f0634aa43044dad2f
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-09-09 16:34:50 +0200

    feat: assets

--- teamhub-frontend/src/components/Cards/contributeCard.tsx
+++ teamhub-frontend/src/components/Cards/contributeCard.tsx
@@ -1,15 +1,12 @@
 export default function ContributeCard() {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-base-100 w-96 shadow-xl m-4">
       <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Title"
-        />
+        <img src=".../assets/colour.jpg" alt="Title" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">Contribute to meeting</h2>
-        <p>
+        <p className="text-left">
           Have a topic to raise? Create a contribution and make sure your voice
           is heard!
         </p>
