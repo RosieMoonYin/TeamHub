ce023dca98e202c5e9a659c3963b619965301e90
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-09-09 15:21:56 +0200

    feat: controller end points for Posts

--- teamhub-backend/Controllers/MeetingController.cs
+++ teamhub-backend/Controllers/MeetingController.cs
@@ -1,69 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
-using teamhub_backend.Models;
-
-namespace teamhub_backend.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class MeetingController : ControllerBase
+namespace teamhub_backend
 {
-    private static List<Meeting> Meetings = new List<Meeting>();
-    private static List<Post> Posts = new List<Post>(); 
-
-    // GET: api/meetings
-    [HttpGet]
-    public ActionResult<IEnumerable<Meeting>> GetMeetings()
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MeetingController : ControllerBase
     {
-        return Ok(Meetings);
-    }
-
-    // GET: api/meetings/{id}
-    [HttpGet("{id}")]
-    public ActionResult<Meeting> GetMeeting(int id)
-    {
-        var meeting = Meetings.FirstOrDefault(m => m.Id == id);
-        if (meeting == null)
+        // GET: api/meetings
+        [HttpGet]
+        public ActionResult<IEnumerable<Meeting>> GetMeetings()
         {
-            return NotFound();
-        }
-        return Ok(meeting);
-    }
-
-    [HttpPost]
-public ActionResult<Meeting> PostMeeting([FromBody] Meeting meeting)
-{
-    meeting.Id = Meetings.Count + 1; 
-    Meetings.Add(meeting);
-    return CreatedAtAction(nameof(GetMeeting), new { id = meeting.Id }, meeting);
-}
-
-
-// DELETE: api/meetings/{id}
-[HttpDelete("{id}")]
-public ActionResult DeleteMeeting(int id)
-{
-        var meeting = Meetings.FirstOrDefault(m => m.Id == id);
-        if (meeting == null)
-        {
-            return NotFound();
+            return Ok(Data.Meetings);
         }
 
-        Meetings.Remove(meeting);
-        return NoContent();
-    }
-
-    // GET: api/meetings/{meetingId}/posts
-    [HttpGet("{meetingId}/posts")]
-    public ActionResult<IEnumerable<Post>> GetPostsForMeeting(int meetingId)
-    {
-        var meeting = Meetings.FirstOrDefault(m => m.Id == meetingId);
-        if (meeting == null)
+        // GET: api/meetings/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Meeting> GetMeeting(int id)
         {
-            return NotFound("Meeting not found.");
+            var meeting = Data.Meetings.FirstOrDefault(m => m.Id == id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+            return Ok(meeting);
         }
 
-        var postsForMeeting = Posts.Where(p => p.MeetingId == meetingId).ToList();
-        return Ok(postsForMeeting);
+        // POST: api/meetings
+        [HttpPost]
+        public ActionResult<Meeting> PostMeeting([FromBody] Meeting meeting)
+        {
+            meeting.Id = Data.Meetings.Count + 1;
+            Data.Meetings.Add(meeting);
+            return CreatedAtAction(nameof(GetMeeting), new { id = meeting.Id }, meeting);
+        }
+
+        // DELETE: api/meetings/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteMeeting(int id)
+        {
+            var meeting = Data.Meetings.FirstOrDefault(m => m.Id == id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            Data.Meetings.Remove(meeting);
+            return NoContent();
+        }
     }
 }
-
---
+++ teamhub-backend/Controllers/PostController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+namespace teamhub_backend
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        // GET: api/posts
+        [HttpGet]
+        public ActionResult<IEnumerable<Post>> GetAllPosts()
+        {
+            return Ok(Data.Posts);
+        }
+
+        // GET: api/posts/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Post> GetPost(int id)
+        {
+            var post = Data.Posts.FirstOrDefault(p => p.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        // POST: api/posts
+        [HttpPost]
+        public ActionResult<Post> CreatePost(Post post)
+        {
+            var meeting = Data.Meetings.FirstOrDefault(m => m.Id == post.MeetingId);
+            if (meeting == null)
+            {
+                return BadRequest("Meeting not found");
+            }
+
+            post.Id = Data.Posts.Count + 1;
+            Data.Posts.Add(post);
+
+            return CreatedAtAction(nameof(GetPost), new { id = post.Id }, post);
+        }
+
+        // DELETE: api/posts/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeletePost(int id)
+        {
+            var post = Data.Posts.FirstOrDefault(p => p.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            Data.Posts.Remove(post);
+            return NoContent();
+        }
+    }
+}
