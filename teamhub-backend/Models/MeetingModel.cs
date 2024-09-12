ee5b7c575efa2b12a79750727bf36910653eb518
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-09-12 16:42:15 +0200

    feat: add data annotations ready for EF core migrations

--- teamhub-backend/Models/MeetingModel.cs
+++ teamhub-backend/Models/MeetingModel.cs
@@ -1,24 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
 namespace teamhub_backend;
 
-    public class Meeting
-    {
-        public int Id { get; set; }
+public class Meeting
+{
+    [Key]
+    public int Id { get; set; }
 
-        public required string MeetingName { get; set; }
+    [Required]
+    [StringLength(300)]
+    public required string MeetingName { get; set; }
 
-        public string? Description { get; set; }
+    [StringLength(500)]
+    public string? Description { get; set; }
 
-        public required DateTime MeetingDate { get; set; }
+    public required DateTime MeetingDate { get; set; }
 
+    [StringLength(2000)]
+    public string? Summary { get; set; }
 
-        public string? Summary { get; set; } 
+    public List<string>? ActionPoints { get; set; } = [];
 
-        public List<String>? ActionPoints { get; set; } = new List<string>();
+    [StringLength(1000)]
+    public string? AISummary { get; set; }
 
-        public string? AISummary { get; set; }
+    [StringLength(100)]
+    public string? Status { get; set; } = "Open";
 
-        public string? Status { get; set; } = "Open";
+    public List<Post>? Posts { get; set; } = [];
 
-        public List<Post>? Posts { get; set; } = new List<Post>();
-    
-    }
+}
--- teamhub-backend/Models/PostModel.cs
+++ teamhub-backend/Models/PostModel.cs
@@ -1,18 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace teamhub_backend;
 public class Post
 {
+
     public int Id { get; set; }
 
+    [Required]
+    [StringLength(100)]
     public required string PersonName { get; set; }
 
+    [Required]
     public required string TextInput { get; set; }
 
+    [StringLength(100)]
     public string PostType { get; set; } = "General";
 
+    [StringLength(100)]
     public string Status { get; set; } = "Not Urgent";
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+    [ForeignKey("MeetingId")]
     public int MeetingId { get; set; } //my foreign key
 
 }
